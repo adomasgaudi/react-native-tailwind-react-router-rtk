a0c5785cc951e4d6f3520978af2a7baa32fa0fa3
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2022-07-31 17:12:08 +0300

    feat: add apiprovider SP2/3

---
+++ src/features/api/apiSlice.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:3500",
+});
+
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery,
+  endpoints: (builder) => ({
+
+    data: builder.query({
+      query: () => '/data',
+    }),
+
+  })
+});
+
+export const {
+  useDataQuery,
+} = apiSlice;
